13feaab96b83393cf1ce23a42c0bf3a3b6e1d0fa
Bruna de Oliveira <bruna.oliveira@mercator.group>
2025-05-23 16:41:07 +0100

    Calculator review minor tweaks

---
+++ src/main/scala/Calculator.scala
@@ -0,0 +1,12 @@
+class Calculator {
+
+  def add(x:Int, y:Int): Int = x + y
+  // ??? It is a placeholder
+
+  def multiply(x:Int, y: Int):Int = x * y
+
+  //TASK:
+
+  // 1. Subtract
+  def subtract(x: Int, y: Int): Int = x - y
+}
